de0c859428b73e7401e43f7833772386ca3afe3d
Enzo MAROS <enzo.maros@gmail.com>
2023-11-12 13:54:03 +0100

    Refactorisation ProjetBookmark et création ButtonImage

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -12,8 +12,7 @@ export default function Button({ children, className, icon, onClick }: Props) {
   return (
     <button
       className={'ease-in-out transition-colors flex gap-3 justify-center min-h-[44px] items-center rounded-xl px-5 py-1 text-left ' + (className ?? '')}
-      onClick={onClick}
-    >
+      onClick={onClick}>
       {(icon) ? <Image src={icon} alt="Icon" width={18} height={18} /> : <></>}
       {children}
     </button>
--- components/project/ProjetBookmark.tsx
+++ components/project/ProjetBookmark.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import bookmarkFill from '@/public/icons/bookmark-fill.svg'
 import bookmarkLine from '@/public/icons/bookmark-line.svg'
+import ButtonImage from "../common/ButtonImage";
 
 type Props = {
   bookmarked?: boolean,
@@ -9,13 +10,6 @@ type Props = {
 
 export default function ProjectBookmark({ bookmarked = false, onBookmark }: Props) {
   return (
-    <button className="relative h-9 w-9 bg-white rounded-full active:scale-90 transition-transform" onClick={() => onBookmark(!bookmarked)}>
-      <Image
-        src={bookmarked ? bookmarkFill : bookmarkLine}
-        alt="Bookmark project"
-        height={20}
-        width={20}
-        className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]" />
-    </button>
+    <ButtonImage image={bookmarked ? bookmarkFill : bookmarkLine} onClick={() => onBookmark(!bookmarked)} />
   )
 }
