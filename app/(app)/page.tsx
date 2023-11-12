7c310d3f449e0fe33f037c146e7be521803ba919
Enzo MAROS <enzo.maros@gmail.com>
2023-11-12 15:09:22 +0100

    Bouton charger plus de projet et API projet

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,20 +1,12 @@
 import SignOut from "@/components/auth/SignOut";
 import ProjectContainer from "@/components/project/ProjectContainer";
 import prisma from "@/lib/prisma";
+import { URL } from "url";
 
 export default async function Home() {
-  const projects = await prisma.project.findMany({
-    skip: 0,
-    take: 12,
-    include: {
-      domain: true,
-      type: true,
-      owner: true,
-    },
-  })
-
+  const url = new URL("/api/project?cursor", process.env.BASE_URL)
   return (
-    <ProjectContainer projects={projects} />
+    <ProjectContainer fetchUrl={url.toString()} />
     // <SignOut />
   )
 }
--- components/project/ProjectContainer.tsx
+++ components/project/ProjectContainer.tsx
@@ -3,6 +3,8 @@
 import useBookmarks from "@/hooks/useBookmarks";
 import { Prisma } from "@prisma/client";
 import ProjectCard from "./ProjectCard";
+import { useEffect, useState } from "react";
+import Button from "../common/Button";
 
 const projectWithCompleteData = Prisma.validator<Prisma.ProjectDefaultArgs>()({
   include: {
@@ -14,26 +16,52 @@ const projectWithCompleteData = Prisma.validator<Prisma.ProjectDefaultArgs>()({
 
 type ProjectWithCompleteData = Prisma.ProjectGetPayload<typeof projectWithCompleteData>
 type Props = {
-  projects: ProjectWithCompleteData[],
+  fetchUrl: string
 }
 
-export default function ProjectContainer({ projects }: Props) {
+export default function ProjectContainer({ fetchUrl }: Props) {
   // Récupérer les éléments dans les bookmarks
   const [bookmarks, setBookmarkStatus] = useBookmarks()
+  const [projects, setProjects] = useState([] as ProjectWithCompleteData[])
+  const [cursor, setCursor] = useState("")
+
+  useEffect(() => {
+    fetch(fetchUrl).then(res => res.json()).then(data => {
+      setCursor(data.cursor)
+      setProjects(data.data)
+    })
+  }, [])
+
+  const loadMore = () => {
+    if (cursor == '')
+      return;
+
+    const url = new URL(fetchUrl)
+    url.searchParams.set('cursor', cursor)
+
+    fetch(url.toString()).then(res => res.json()).then(data => {
+      setCursor(data.cursor)
+      setProjects([...projects, ...data.data])
+    })
+  }
 
   return (
-    <div className="flex flex-wrap gap-x-10 gap-y-6 justify-stretch">
-      {projects.map(project => {
-        const bookmarked = bookmarks.map(b => b.id).includes(project.id)
-        return (
-          <div className="grow shrink min-w-[330px] basis-0" key={project.id}>
-            <ProjectCard
-              project={project}
-              bookmarked={bookmarked}
-              onBookmark={(value) => setBookmarkStatus(project, value)} />
-          </div>
-        )
-      })}
+    <div className="my-5">
+      <div className="flex flex-wrap gap-x-10 gap-y-6 justify-stretch">
+        {projects.map(project => {
+          const bookmarked = bookmarks.map(b => b.id).includes(project.id)
+          return (
+            <div className="grow shrink min-w-[330px] basis-0" key={project.id}>
+              <ProjectCard
+                project={project}
+                bookmarked={bookmarked}
+                onBookmark={(value) => setBookmarkStatus(project, value)} />
+            </div>
+          )
+        })}
+      </div>
+
+      <Button className="bg-gray-200 mx-auto mt-10 hover:bg-gray-300" onClick={() => loadMore()}>Charger plus</Button>
     </div>
   )
 }
