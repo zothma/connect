cb972f7289212f158c35fba3cd819b18e2306ed1
Enzo MAROS <enzo.maros@gmail.com>
2023-11-30 13:13:32 +0100

    useToast, useFetch and Providers

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,4 +1,5 @@
 import HeaderBar from "@/components/navigation/HeaderBar";
+import ToastContainer from "@/components/toast/ToastContainer";
 import React from "react";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
@@ -7,6 +8,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <div className="h-[100dvh] overflow-scroll scroll-smooth pb-16 md:pb-0 md:pt-16 md:scroll-pt-16">
         <HeaderBar />
         <div className="mx-auto w-[95%] max-w-7xl h-full">
+          <ToastContainer />
           {children}
         </div>
       </div>
--- hooks/useBookmarks.ts
+++ hooks/useBookmarks.ts
@@ -1,31 +1,11 @@
-import { fetchWithTimeout } from '@/lib/async'
 import { ApiBookmarkReturnType } from '@/types/api'
 import { ProjectWithCompleteData } from '@/types/models'
 import { useEffect, useState } from 'react'
+import useFetch from './useFetch'
 
 type ProjectList = ProjectWithCompleteData[]
 type ApiCallCallback = () => void
 type ApiCallMethod = 'POST' | 'DELETE'
-
-/**
- * Wrapper around the custom fetch to make a bookmark API call
- * @param project
- * @param method
- * @param callback
- */
-async function makeApiCall(
-  project: ProjectWithCompleteData,
-  method: ApiCallMethod,
-  callback: ApiCallCallback
-) {
-  try {
-    const url = `/api/bookmark/${project.id}`
-    const result = await fetchWithTimeout(url, { method })
-    if (result.ok) callback()
-  } catch (error) {
-    console.error('Error fetching data: ', error)
-  }
-}
 
 /**
  * Hook allowing to fetch and manipulate (add and remove) bookmarks.
@@ -37,15 +17,36 @@ export default function useBookmarks(): [
   (project: ProjectWithCompleteData, status: boolean) => void
 ] {
   const [bookmarks, setBookmarks] = useState<ProjectList>([])
+  const fetch = useFetch()
 
   /**
    * Fetches bookmarked projects
    */
   const fetchData = async () => {
     try {
-      const result = await fetchWithTimeout('/api/bookmark')
+      const result = await fetch('/api/bookmark')
       const data: ApiBookmarkReturnType = await result.json()
       setBookmarks(data.data)
+    } catch (error) {
+      console.error('Error fetching data: ', error)
+    }
+  }
+
+  /**
+   * Wrapper around the custom fetch to make a bookmark API call
+   * @param project
+   * @param method
+   * @param callback
+   */
+  async function makeApiCall(
+    project: ProjectWithCompleteData,
+    method: ApiCallMethod,
+    callback: ApiCallCallback
+  ) {
+    try {
+      const url = `/api/bookmark/${project.id}`
+      const result = await fetch(url, { method })
+      if (result.ok) callback()
     } catch (error) {
       console.error('Error fetching data: ', error)
     }
