0b769048a5cbc5f83980fef89d23298599eb4305
Enzo MAROS <enzo.maros@gmail.com>
2023-12-25 19:27:54 +0100

    New professional bridge page

---
+++ app/(app)/discover/[bridgeSlug]/page.tsx
@@ -0,0 +1,32 @@
+import AdvertContainer from '@/components/advert/AdvertContainer'
+import HeaderLayout from '@/components/layout/HeaderLayout'
+import prisma from '@/lib/prisma'
+import { slugify } from '@/lib/string'
+import { notFound } from 'next/navigation'
+
+type HeaderProps = React.ComponentPropsWithoutRef<typeof HeaderLayout>
+type HeaderOptions = HeaderProps['headerOptions']
+type Params = { bridgeSlug: string }
+type Props = { params: Params }
+
+export default async function BridgePage({ params }: Props) {
+  const { bridgeSlug } = params
+
+  // Ensure professional bridge is existing
+  const bridges = await prisma.professionalBridge.findMany()
+  const currentBridge = bridges.filter((b) => slugify(b.name) == bridgeSlug)
+  if (currentBridge.length != 1) return notFound()
+
+  // Prepare the adverts
+  const url = new URL('/api/advert', process.env.BASE_URL)
+  const headerOptions: HeaderOptions = {
+    title: currentBridge[0].name,
+    description: currentBridge[0].description,
+  }
+
+  return (
+    <HeaderLayout headerOptions={headerOptions}>
+      <AdvertContainer fetchUrl={url.toString()} />
+    </HeaderLayout>
+  )
+}
--- app/(app)/discover/page.tsx
+++ app/(app)/discover/page.tsx
@@ -1,17 +1,19 @@
-import AdvertContainer from "@/components/advert/AdvertContainer";
-import HeaderLayout from "@/components/layout/HeaderLayout";
-import React from "react";
+import AdvertContainer from '@/components/advert/AdvertContainer'
+import HeaderLayout from '@/components/layout/HeaderLayout'
+import React from 'react'
 
-type HeaderOptions = React.ComponentPropsWithoutRef<typeof HeaderLayout>["headerOptions"]
+type HeaderProps = React.ComponentPropsWithoutRef<typeof HeaderLayout>
+type HeaderOptions = HeaderProps['headerOptions']
 
 export default function DiscoverPage() {
-  const url = new URL("/api/advert", process.env.BASE_URL)
+  const url = new URL('/api/advert', process.env.BASE_URL)
   const headerOptions: HeaderOptions = {
-    title: "Découvrir",
-    description: "Parcourez les annonces et profils qui "
-      + "vous correspondent le plus, ou laissez MeetAct "
-      + "vous emporter vers ce qui se fait de mieux dans "
-      + "chaque domaine."
+    title: 'Découvrir',
+    description:
+      'Parcourez les annonces et profils qui ' +
+      'vous correspondent le plus, ou laissez MeetAct ' +
+      'vous emporter vers ce qui se fait de mieux dans ' +
+      'chaque domaine.',
   }
 
   return (
